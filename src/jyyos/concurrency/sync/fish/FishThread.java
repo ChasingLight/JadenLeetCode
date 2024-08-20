7a234963041e1a58bfb4641670053d85a97b6a00
jinhaodong <657091714@qq.com>
2024-08-20 11:11:38 +0800

    jyy-os  条件变量面试题，实现  * 描述：  *      有三种线程：  *      1> Ta若干，死循环打印 < ;  *      2> Ta若干，死循环打印 > ;  *      3> Ta若干，死循环打印 _ 。  * 任务：  *      对线程同步，使得屏幕打印出 <><_ 和  ><>_  * Tips:  *      使用条件变量，只要回答三个问题：  *      打印 < 的条件？ 打印 > 的条件？ 打印 _ 的条件？

---
+++ src/jyyos/concurrency/sync/fish/FishThread.java
@@ -0,0 +1,64 @@
+package jyyos.concurrency.sync.fish;
+
+/**
+ * @author jinhaodong
+ * @date 2024/8/20 10:06
+ */
+public class FishThread implements Runnable{
+
+    // 初始状态
+    private String currentState = "G";
+
+    private Object lock = new Object();
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        if ("a".equals(name)){
+            while (true){
+                synchronized (lock){
+                    boolean aCanPrint = ("G".equals(currentState) || "D".equals(currentState) || "B".equals(currentState));
+                    if (!aCanPrint){
+                        continue;
+                    }
+                    System.out.print("<");
+                    if ("G".equals(currentState)){
+                        currentState = "A";
+                    }else if ("D".equals(currentState)){
+                        currentState = "E";
+                    }else if ("B".equals(currentState)){
+                        currentState = "C";
+                    }
+                }
+            }
+        }else if ("b".equals(name)){
+            while (true){
+                synchronized (lock){
+                    boolean bCanPrint = ("G".equals(currentState) || "A".equals(currentState) || "E".equals(currentState));
+                    if (!bCanPrint){
+                        continue;
+                    }
+                    System.out.print(">");
+                    if ("G".equals(currentState)){
+                        currentState = "D";
+                    }else if ("A".equals(currentState)){
+                        currentState = "B";
+                    }else if ("E".equals(currentState)){
+                        currentState = "F";
+                    }
+                }
+            }
+        }else if ("c".equals(name)){
+            while (true){
+                synchronized (lock){
+                    boolean cCanPrint = ("C".equals(currentState) || "F".equals(currentState));
+                    if (!cCanPrint){
+                        continue;
+                    }
+                    System.out.print("_");
+                    currentState = "G";
+                }
+            }
+        }
+    }//end method
+}//end class
