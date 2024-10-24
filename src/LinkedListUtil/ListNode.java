265bb345139ebee964524bdaa2535c4653892078
jinhaodong <657091714@qq.com>
2024-10-24 11:49:27 +0800

    pref：优化单向链表定义、构建工具类，相关代码。

--- src/LinkedListUtil/ListNode.java
+++ src/LinkedListUtil/ListNode.java
@@ -1,10 +1,13 @@
 package LinkedListUtil;
 
+/**
+ * 单向链表定义
+ */
 public class ListNode {
 	public int val;  
     public ListNode next;  //默认值为null，正好符合链表的特点
-	public ListNode(int x) { 
-		val = x; 
+	public ListNode(int val) {
+		this.val = val;
 	}
 
 }
--- src/LinkedListUtil/ListNodeUtil.java
+++ src/LinkedListUtil/ListNodeUtil.java
@@ -4,45 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListNodeUtil {
-	
-	/**
-	 * 根据leetcode的ListNode定义，构建链表
-	 * @param input
-	 * @return
-	 */
-	public static ListNode buildListNode(int[] input){
-        ListNode first = null,last = null,newNode;
-        if(input.length > 0){
-            for(int i=0;i<input.length;i++){
-                newNode=new ListNode(input[i]);
-                newNode.next = null;
-                if(first==null){
-                    first=newNode;
-                    last=newNode;
-                }
-                else{
-                    last.next=newNode;
-                    last=newNode;
-                }
 
+    /**
+     * 构建链表
+     * @param input 整数数组
+     * @return
+     */
+    public static ListNode buildListNode(int[] input) {
+        ListNode head = null, last = null, newNode;
+        for (int val : input) {
+            newNode = new ListNode(val);
+            if (head == null) {
+                head = newNode;
+                last = newNode;
+            } else {
+                last.next = newNode;
+                last = newNode;
             }
         }
-        return first;
-    }
-	
-	
-	public static List<Integer> showListNode(ListNode head){
-		List<Integer> output = new ArrayList<>();
-		
-		if(head == null){
-			return null;
-		}
-		
-		while(head != null){
-			output.add(head.val);
-			head = head.next;
-		}
-		
-		return output;
-	}
+        return head;
+    }//end method
+
+    /**
+     * 展示链表信息
+     * @param head  头节点
+     * @return
+     */
+    public static List<Integer> showListNode(ListNode head) {
+        List<Integer> output = new ArrayList<>();
+        if (head == null) {
+            return null;
+        }
+        while (head != null) {
+            output.add(head.val);
+            head = head.next;
+        }
+        return output;
+    }//end method
 }
