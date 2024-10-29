dcb19d363819d60785c05073b7422c22b38105f5
jinhaodong <657091714@qq.com>
2024-10-29 14:54:38 +0800

    feat(精简main测试类)

--- src/MainTest.java
+++ src/MainTest.java
@@ -9,11 +9,12 @@ import TreeUtil.TreeNodeUtil;
 public class MainTest {
 
     public static void main(String[] args) {
-        Integer[] preOrder = {3, 9, null, null, 20, 15, null, null, 7, null, null};
+        /*Integer[] preOrder = {3, 9, null, null, 20, 15, null, null, 7, null, null};
         TreeNode root = TreeNodeUtil.buildBinaryTree(preOrder);
         System.out.println(TreeNodeUtil.preorderTraversal(root));
         System.out.println(TreeNodeUtil.inorderTraversal(root));
-        System.out.println(TreeNodeUtil.levelOrder(root));
+        System.out.println(TreeNodeUtil.levelOrder(root));*/
+
     }
 
 }
