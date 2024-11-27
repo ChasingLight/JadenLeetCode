6b724a3e73e480941593a060fb6384e113c5dfaa
jinhaodong <657091714@qq.com>
2024-11-27 11:20:12 +0800

    2462 雇佣 K 位工人的总代价（解法2：剪枝、2个小顶堆）

--- src/MainTest.java
+++ src/MainTest.java
@@ -7,26 +7,12 @@ import java.util.*;
 public class MainTest {
 
     public static void main(String[] args) {
-        /*int[] costs = {17, 12, 10, 2, 7, 2, 11, 20, 8};
-        int k = 3;
-        int candidates = 4;
-        System.out.println(totalCost(costs, k, candidates));*/
+        int[] costs = {2,1,2};
+        int k = 1;
+        int candidates = 1;
+        System.out.println(totalCost(costs, k, candidates));
 
     }
-
-
-    public int findKthLargest(int[] nums, int k) {
-        // 小根堆
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int num : nums) {
-            pq.offer(num);
-            if (pq.size() > k) {
-                pq.poll();
-            }
-        }
-        return pq.peek();
-    }
-
 
 
     /**
@@ -36,43 +22,41 @@ public class MainTest {
      * 1 <= costs[i] <= 10^5
      * 1 <= k, candidates <= costs.length
      */
-    public static long totalCost(int[] costs, int k, int candidates) {
-        // 1个优先级队列 + 每个元素是长度为2的数组（下标0记录值、下标1记录值对应下标）
-        // 值不同时，降序排序；如果值相同时，将下标降序排序。
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
-        // 初始化：是否覆盖全量数据
+    public static long totalCost(int[] costs, int k, int candidates){
+        // 解法2：剪枝、2个小顶堆、2个队头比较
+        long res = 0L;
         int n = costs.length;
-        int left = candidates - 1;
-        int right = n - candidates;
-        if (left + 1 < right) {
-            for (int i = 0; i <= left; i++) {
-                pq.offer(new int[]{costs[i], i});
+        // 可全量覆盖 costs 所有元素
+        if (k >= 1 + n - 2*candidates) {
+            Arrays.sort(costs);
+            for (int i = 0; i < k; i++) {
+                res += costs[i];
             }
-            for (int i = right; i < n; i++) {
-                pq.offer(new int[]{costs[i], i});
-            }
-        }else{
-            // 覆盖全量数据
-            for (int i = 0; i < n; i++) {
-                pq.offer(new int[]{costs[i], i});
-            }
+            return res;
+        }
+        // 未全量覆盖，初始化
+        PriorityQueue<Integer> leftPq = new PriorityQueue<>();
+        for (int i = 0; i < candidates; i++) {
+            leftPq.offer(costs[i]);
+        }
+        PriorityQueue<Integer> rightPq = new PriorityQueue<>();
+        for (int i = n-candidates; i < n; i++) {
+            rightPq.offer(costs[i]);
         }
         // k 轮雇佣
-        long res = 0;
+        int left = candidates - 1;
+        int right = n - candidates;
         for (int i = 0; i < k; i++) {
-            int[] arr = pq.poll();
-            int cost = arr[0];
-            int index = arr[1];
-            res += cost;
-            if (left + 1 < right) {
-                if (index <= left) {
-                    ++left;
-                    pq.offer(new int[]{costs[left], left});
-                }else if (index >= right) {
-                    --right;
-                    pq.offer(new int[]{costs[right], right});
-                }
+            // 代价相同，选左侧下标更小
+            int cost;
+            if (leftPq.peek() <= rightPq.peek()){
+                cost = leftPq.poll();
+                leftPq.offer(costs[++left]);
+            }else{
+                cost = rightPq.poll();
+                rightPq.offer(costs[--right]);
             }
+            res += cost;
         }
         return res;
     }//end method
