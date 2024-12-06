c800aab79d83fb1717ac0438914c1ecd82ca7cdc
jinhaodong <657091714@qq.com>
2024-12-06 16:56:37 +0800

    739 每日温度（单调栈）

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * @author jinhaodong
@@ -8,25 +7,41 @@ import java.util.stream.Collectors;
 public class MainTest {
 
     public static void main(String[] args) {
-        int[] arr = {2,1,3};
-        int n = arr.length;
-        // 2,0,1
-        // 对数组下标（即-下标对应的值）进行排序，不改变原数组的顺序
-        // 方法1：Arrays.sort
-        Integer[] ids = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            ids[i] = i;
-        }
-        Arrays.sort(ids, (i,j) -> arr[j]-arr[i]);
-        System.out.println(Arrays.toString(ids));
-
-        // 方法2：Map => index,value
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(i, arr[i]);
-        }
-        List<Integer> res = map.keySet().stream().sorted((i, j) -> map.get(j) - map.get(i)).collect(Collectors.toList());
-        System.out.println(res.toString());
+        int[] temperatures = {1,4,3,5,5,2,3,6};
+        System.out.println( Arrays.toString(dailyTemperatures(temperatures)) );
     }
+
+    /**
+     * 每日温度
+     * @param temperatures  数组长度取值范围 [1,10^5]
+     * @return
+     */
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        /*// 从右往左
+        for (int i = n - 1; i >= 0; i--) {
+            // 当前值大于等于栈顶值，栈顶值出栈
+            while(!stack.isEmpty()
+                    && temperatures[i] >= temperatures[stack.peek()] ) {
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                res[i] = stack.peek() - i;
+            }
+            stack.push(i);
+        }*/
+
+        // 从左往右
+        for (int i = 0; i < n; i++) {
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int j = stack.pop();
+                res[j] = i - j;
+            }
+            stack.push(i);
+        }
+        return res;
+    }//end method
 
 }
