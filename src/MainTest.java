284c7ba5cc7c5c091318ade1e4b83c7c352d43a8
jinhaodong <657091714@qq.com>
2024-12-09 17:01:14 +0800

    167. 两数之和 II - 输入有序数组 有序 + 相向双指针

--- src/MainTest.java
+++ src/MainTest.java
@@ -7,13 +7,28 @@ import java.util.*;
 public class MainTest {
 
     public static void main(String[] args) {
-        StockSpanner sp = new StockSpanner();
-        System.out.println(sp.next(100));
-        System.out.println(sp.next(80));
-        System.out.println(sp.next(60));
-        System.out.println(sp.next(70));
-        System.out.println(sp.next(60));
-        System.out.println(sp.next(75));
-        System.out.println(sp.next(85));
+        int[] numbers = {-1,0};
+        int target = -1;
+        System.out.println(Arrays.toString(twoSum(numbers, target)));
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+        // 相向双指针
+        int n = numbers.length;
+        int i = 0, j = n - 1;
+        while (i < j) {
+            int value = numbers[i] + numbers[j];
+            if (value > target) {
+                j--;
+            }else if (value < target) {
+                i++;
+            }else{
+                res[0] = ++i;
+                res[1] = ++j;
+                break;
+            }
+        }
+        return res;
     }
 }
