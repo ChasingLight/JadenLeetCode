c6d30cd25a9569f024e4ef9dffee886f6d8e0faa
jinhaodong <657091714@qq.com>
2024-12-10 09:43:44 +0800

    11. 盛最多水的容器（相向双指针）     leetcode 书写了简单证明题解

--- src/MainTest.java
+++ src/MainTest.java
@@ -7,51 +7,24 @@ import java.util.*;
 public class MainTest {
 
     public static void main(String[] args) {
-        int[] nums = {-1,0,1,2,-1,-4};
-        System.out.println(threeSum(nums));
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
     }
 
 
-    public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        // 升序排序
-        Arrays.sort(nums);
-        // 三数之和
-        int n = nums.length;
-        for (int i = 0; i <= n - 3; i++) {
-            int x = nums[i];
-            if (i > 0 && x == nums[i-1]){
-                continue;
+    public static int maxArea(int[] height) {
+        int res = 0;
+        int n = height.length;
+        int i = 0, j = n - 1;
+        while(i < j){
+            res = Math.max(res, Math.min(height[i], height[j]) * (j - i));
+            if(height[i] < height[j]){
+                i++;
+            }else{
+                j--;
             }
-            // 剪枝
-            if (x + nums[i+1] +nums[i+2] > 0){
-                break;
-            }
-            if (x + nums[n-2] + nums[n-1] < 0){
-                continue;
-            }
-            int j = i + 1;
-            int k = n - 1;
-            // 两数之和
-            while(j < k){
-                int value = x + nums[j] + nums[k];
-                if(value > 0){
-                    k--;
-                }else if (value < 0){
-                    j++;
-                }else{
-                    res.add(Arrays.asList(x, nums[j], nums[k]));
-                    j++;
-                    while (j < k && nums[j] == nums[j-1]){
-                        j++;
-                    }
-                    k--;
-                    while(j < k && nums[k] == nums[k+1]){
-                        k--;
-                    }
-                }
-            }//end while
         }
         return res;
-    }//end method
+    }
+
 }
