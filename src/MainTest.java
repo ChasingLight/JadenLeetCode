b2807ec01dd30761a1f2964e17413be7d09ef311
jinhaodong <657091714@qq.com>
2024-12-10 14:48:37 +0800

    42. 接雨水（相向双指针-解法）    说明：相比于 前后缀解法，空间负责度从 O(n) 优化为了 O(1)。
       【相向双指针】
       前提：我们已经算出来，一部分前缀最大值 preMax，一部分后缀的最大值 sufMax
       1> preMax < sufMax
       左边木桶的容量 就是 preMax，因为 min(preMax, >=sufMax) = preMax
       计算完毕，向右继续遍历
       2> preMax >= sufMax
       右边木桶的容量 就是 sufMax，因为 min(>=preMax, sufMax) = sufMax
       计算完毕，向左继续遍历

--- src/MainTest.java
+++ src/MainTest.java
@@ -15,20 +15,18 @@ public class MainTest {
     public static int trap(int[] height) {
         int res = 0;
         int n = height.length;
-        // 前缀最大高度
-        int[] preMax = new int[n];
-        preMax[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            preMax[i] = Math.max(preMax[i-1], height[i]);
-        }
-        // 后缀最大高度
-        int[] sufMax = new int[n];
-        sufMax[n-1] = height[n-1];
-        for (int i = n-2; i >= 0; i--) {
-            sufMax[i] = Math.max(sufMax[i+1], height[i]);
-        }
-        for (int i = 0; i < n; i++) {
-            res += Math.min(preMax[i], sufMax[i]) - height[i];
+        // 相向双指针
+        int left = 0, right = n -1;
+        int preMax = 0;
+        int sufMax = 0;
+        while(left < right){
+            preMax = Math.max(preMax, height[left]);
+            sufMax = Math.max(sufMax, height[right]);
+            if(preMax < sufMax){
+                res += preMax - height[left++];
+            }else{
+                res += sufMax - height[right--];
+            }
         }
         return res;
     }//end method
