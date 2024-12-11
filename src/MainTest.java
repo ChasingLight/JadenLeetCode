571146050a46c40b3aa25d720a01ab33e2b4f300
jinhaodong <657091714@qq.com>
2024-12-11 16:04:36 +0800

    2958. 最多 K 个重复元素的最长子数组（滑动窗口法-虫取法）       使用 Map 记录元素出现的频率。

--- src/MainTest.java
+++ src/MainTest.java
@@ -7,41 +7,32 @@ import java.util.*;
 public class MainTest {
 
     public static void main(String[] args) {
-        String s = "bbbbb";
-        System.out.println(lengthOfLongestSubstring(s));
+        int[] nums = {5,5,5,5,5,5,5};
+        int k = 4;
+        System.out.println(maxSubarrayLength(nums, k));
     }
 
     /**
-     * 0 <= s.length <= 5 * 104
-     * s 由英文字母、数字、符号和空格组成
-     * @param s
-     * @return
+     * 1 <= nums.length <= 105
+     * 1 <= nums[i] <= 109
+     * 1 <= k <= nums.length
      */
-    public static int lengthOfLongestSubstring(String s) {
-        int res = 0;
-        int n = s.length();
+    public static int maxSubarrayLength(int[] nums, int k) {
+        int res = 1;
+        int n = nums.length;
         int left = 0;
-        String currStr;
+        Map<Integer, Integer> map = new HashMap<>();
         for (int right = 0; right < n; right++) {
-            currStr = s.substring(left, right + 1);
-            while(hasRepeatedChar(currStr)) {
-                // 收缩左指针
-                currStr = s.substring(++left, right + 1);
+            int x = nums[right];
+            map.put(x, map.getOrDefault(x, 0) + 1);
+            // 收缩左指针
+            while(map.get(x) > k) {
+                map.put(nums[left], map.get(nums[left]) - 1);
+                left++;
             }
             res = Math.max(res, right - left + 1);
         }
         return res;
     }//end method
-
-    private static boolean hasRepeatedChar(String s) {
-        int n = s.length();
-        if (1 == n){
-            return false;
-        }else{
-            char c = s.charAt(n - 1);
-            String lastStr = s.substring(0, n - 1);
-            return lastStr.indexOf(c) != -1;
-        }
-    }
 
 }
