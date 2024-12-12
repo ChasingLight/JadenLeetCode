7e80dbb2b15425bef6b0653c026a30b877156103
jinhaodong <657091714@qq.com>
2024-12-12 08:45:38 +0800

    2779. 数组的最大美丽值（排序 + 滑动窗口）
    因为求的是最长子序列长度，子序列而非子数组，所以排序不影响最终的结果。
    
    y - x > 2k时，说明 伸伸手，拉不住（无交集），需要收缩左指针。

--- src/MainTest.java
+++ src/MainTest.java
@@ -7,42 +7,28 @@ import java.util.*;
 public class MainTest {
 
     public static void main(String[] args) {
-        String s = "24489929009";
-        System.out.println(longestSemiRepetitiveSubstring(s));
+        int[] nums = {4,6,1,2};
+        int k = 2;
+        System.out.println(maximumBeauty(nums, k));
     }
 
     /**
-     * 1 <= s.length <= 50
-     * '0' <= s[i] <= '9'
+     * 1 <= nums.length <= 10^5
+     * 0 <= nums[i], k <= 10^5
      */
-    public static int longestSemiRepetitiveSubstring(String s) {
-        int n = s.length();
-        if (n == 1){
-            return 1;
-        }
-        // 滑动窗口
+    public static int maximumBeauty(int[] nums, int k) {
         int res = 1;
+        int n = nums.length;
+        // 排序 + 滑动窗口
+        Arrays.sort(nums);
         int left = 0;
-        int pairs = 0;
-        int[] pairValue = new int[2];
-        for (int right = 1; right < n; right++) {
-            // 判断相邻字符是否相等
-            char preChar = s.charAt(right - 1);
-            char c = s.charAt(right);
-            if (preChar == c){
-                pairValue[pairs++] = c;
-            }
-            while(pairs > 1){
+        for (int right = 0; right < n; right++) {
+            while(nums[right] - nums[left] > 2*k){  // 伸伸手，拉不住（无交集）
                 // 收缩左指针
-                if(s.charAt(left) == pairValue[0] && s.charAt(left+1) == pairValue[0]){
-                    pairs--;
-                    pairValue[0] = pairValue[1];
-                }
                 left++;
             }
             res = Math.max(res, right - left + 1);
         }
         return res;
     }//end method
-
 }
