a58f6c7e94b7bf4ee9ff4f9ec0ab21bf58ae229c
jinhaodong <657091714@qq.com>
2024-12-18 16:28:23 +0800

    76. 最小覆盖子串（滑动窗口 + HashMap）
    待思考，如何在O(m+n) 时间复杂度下解决，同时使用 int[] 数组来表示 字母出现的次数？

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,3 +1,5 @@
+import org.omg.PortableInterceptor.INACTIVE;
+
 import java.util.*;
 
 /**
@@ -7,15 +9,14 @@ import java.util.*;
 public class MainTest {
 
     public static void main(String[] args) {
-        int[] nums = {2,4};
-        int x = 6;
-        System.out.println(minOperations(nums, x));
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        System.out.println(minWindow(s, t));
 
         char c1 = 'a';
         char c2 = 'z';
         char c3 = 'A';
         char c4 = 'Z';
-        nums[c1] = 1;
         System.out.println(c1 - 97);
         System.out.println(c2 - 122);
         System.out.println(c3 - 65);
@@ -27,32 +28,46 @@ public class MainTest {
      * 1 <= nums[i] <= 10^4
      * 1 <= x <= 10^9
      */
-    public static int minOperations(int[] nums, int x) {
-        int res = -1;
-        int n = nums.length;
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
+    public static String minWindow(String s, String t) {
+        // 窗口条件
+        Map<Character, Integer> tMap = new HashMap<>(52);
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
         }
-        // 剪枝：全部移除也无法满足要求
-        if (totalSum < x) {
-            return -1;
-        }
-        // 极限情况: totalSum == x, target = 0 符合
-        int target = totalSum - x;
-        int sum = 0;
+        // 滑动窗口
+        String res = "";
         int left = 0;
+        int n = s.length();
+        Map<Character, Integer> sMap = new HashMap<>(52);
         for (int right = 0; right < n; right++) {
-            sum += nums[right];
-            // 不满足条件 到 满足条件
-            while(sum > target) {
-                // 收缩左指针
-                sum -= nums[left++];
-            }
-            if (sum == target) {
-                res = Math.max(res, right - left + 1);
-            }
+            char c = s.charAt(right);
+            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+            // 满足 到 不满足
+            while(meetRequire(sMap, tMap)){
+                if (res.equals("")){
+                    res = s.substring(left, right+1);
+                }else{
+                   res = (right-left+1) < res.length() ? s.substring(left, right+1) : res;
+                }
+                // 收缩左指针  收缩到位
+                sMap.put(s.charAt(left), sMap.get(s.charAt(left)) - 1);
+                left++;
+            }//end while
         }
-        return res == -1 ? -1 : n-res;
+        // 最小窗口，截取 left、right
+        return res;
     }//end method
+
+        static boolean meetRequire(Map<Character, Integer> sMap, Map<Character, Integer> tMap) {
+            for (Character c :  tMap.keySet()){
+                int tVal = tMap.get(c);
+                Integer sVal = sMap.getOrDefault(c, 0);
+                if (sVal < tVal) {
+                    return false;
+                }
+            }
+            return true;
+        }//end method
+
 }
